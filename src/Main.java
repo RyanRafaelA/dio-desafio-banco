0eb712d2f97a4182964ac91c4cbe28aae591c7fc
RyanRafaelA <ryrafael461@gmail.com>
2025-02-24 23:08:43 -0300

    Criação do extratoBancarios na classe conta e testes no Main concluidos

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,67 @@
+import model.banco.Conta;
 import service.ClienteService;
 import service.ContaService;
 
+import java.util.Scanner;
+
 public class Main {
+    static ClienteService cl = new ClienteService();
+    static ContaService co = new ContaService();
+
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Conta conta;
+
+        int opcao = 1, numeroBancoEnviar;
+        double valor;
+
         bancoDeDadosNaMemoria();
+
+        cl.criarCliente("Ryan Rafael", "17/06/2001", "2758907", 'm');
+        co.criarConta('p', cl.retornaCliente("Ryan Rafael"), 900);
+
+        conta = co.buscaConta(5);
+
+        while(opcao!=6){
+            System.out.println("===========================================");
+            menu();
+            opcao = sc.nextInt();
+
+            switch (opcao){
+                case 1:
+                    System.out.print("R$ ");
+                    valor = sc.nextDouble();
+                    conta.depositar(valor);
+                    break;
+                case 2:
+                    System.out.print("R$ ");
+                    valor = sc.nextDouble();
+                    conta.sacar(valor);
+                    break;
+                case 3:
+                    co.mostraContasCadastradas();
+                    break;
+                case 4:
+                    sc.nextLine();
+                    System.out.print("Numero para transferir: ");
+                    numeroBancoEnviar = sc.nextInt();
+                    System.out.print("R$ ");
+                    valor = sc.nextDouble();
+                    conta.transferir(valor, co.buscaConta(numeroBancoEnviar));
+                    break;
+                case 5:
+                    conta.mostrarExtrato();
+                    break;
+                case 6:
+                    System.out.println("Volte sempre!");
+                    break;
+                default:
+                    System.out.println("Opção errada!");
+            }
+        }
     }
 
     public static void bancoDeDadosNaMemoria()  {
-        ClienteService cl = new ClienteService();
-        ContaService co = new ContaService();
-
         cl.criarCliente("Leonardo", "21/07/1976", "96745754", 'm');
         cl.criarCliente("Fernanda", "01/01/1981", "6857866", 'f');
         cl.criarCliente("Ruan", "18/10/2007", "9664779",'m');
@@ -22,6 +74,7 @@ public class Main {
     }
 
     public static void menu(){
-        System.out.println("");
+        System.out.print("[1] - Depositar\n[2] - Sacar\n[3] - Clientes do banco\n[4] - Transferir\n[5] - Ver Extrato\n[6] - sair" +
+                "\nDigite a opção");
     }
 }
--- src/model/banco/Conta.java
+++ src/model/banco/Conta.java
@@ -2,6 +2,9 @@ package model.banco;
 
 import model.cliente.Cliente;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Conta {
     private final static int AGENCIA_PADRAO = 1;
     private static int SEQUENCIAL = 1;
@@ -10,12 +13,15 @@ public abstract class Conta {
     private int numero;
     protected double saldo;
     private Cliente cliente;
+    private List<String> extratoBancario;
 
     public Conta(Cliente novoCliente, double saldoInicial){
         this.cliente = novoCliente;
         this.saldo = saldoInicial;
         agencia = AGENCIA_PADRAO;
         numero = SEQUENCIAL++;
+
+        extratoBancario = new ArrayList<>();
     }
 
     public int getAgencia(){ return agencia; }
@@ -24,15 +30,27 @@ public abstract class Conta {
 
     public void sacar(double valor) {
         saldo -= valor;
+        registrarExtratoBancario("Sacou R$"+String.format("%.2f",valor));
     }
 
     public void depositar(double valor){
         saldo += valor;
+        registrarExtratoBancario("Depositou R$"+String.format("%.2f",valor));
     }
 
     public void transferir(double valor, Conta contaDestino){
         sacar(valor);
         contaDestino.depositar(valor);
+        registrarExtratoBancario("tranferio para conta "+contaDestino.getNumero()+" R$"+String.format("%.2f",valor));
+    }
+
+    private void registrarExtratoBancario(String registro){
+        extratoBancario.add(registro);
+    }
+
+    public void mostrarExtrato(){
+        System.out.println("===========================================");
+        extratoBancario.forEach(System.out::println);
     }
 
     @Override
